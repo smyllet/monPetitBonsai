0eecba7ee0b9fdb165f9992b5891e96c996a05e3
Smyllet <smyllet.gamer@gmail.com>
2021-11-19 16:06:03 +0100

    feat : sql : pruning

--- src/main/resources/sql/createTable.sql
+++ src/main/resources/sql/createTable.sql
@@ -1,3 +1,4 @@
+drop table if exists monPetitBonsai.pruning;
 drop table if exists monPetitBonsai.watering;
 drop table if exists monPetitBonsai.bonsai;
 drop table if exists monPetitBonsai.owner;
@@ -28,6 +29,13 @@ create table monPetitBonsai.watering (
     foreign key (bonsai_id) references monPetitBonsai.bonsai(id)
 );
 
+create table monPetitBonsai.pruning (
+     id UUID primary key not null,
+     datetime timestamp not null,
+     bonsai_id UUID not null,
+     foreign key (bonsai_id) references monPetitBonsai.bonsai(id)
+);
+
 insert into monPetitBonsai.owner(id, name) values ('a3387036-4946-11ec-81d3-0242ac130003', 'Bryan'), ('ae1f28b4-4946-11ec-81d3-0242ac130003', 'Nahoufane');
 
 insert into monPetitBonsai.bonsai(id, name, species, acquisition_date, acquisition_age, owner_id, status) values
@@ -39,4 +47,7 @@ insert into monPetitBonsai.watering(id, datetime, bonsai_id) VALUES
     ('7e46fc96-4948-11ec-81d3-0242ac130003', '2021-11-08 15:44:00', '63696432-01a2-4b43-b162-47b4f1e6062a'),
     ('7e46fe08-4948-11ec-81d3-0242ac130003', '2021-11-08 22:19:00', '21ea2a5a-a7d0-436f-b40b-a369f00748f6'),
     ('7e46fed0-4948-11ec-81d3-0242ac130003', '2021-11-15 05:54:00', '63696432-01a2-4b43-b162-47b4f1e6062a'),
-    ('7e46fff2-4948-11ec-81d3-0242ac130003', '2021-11-18 03:31:00', '21ea2a5a-a7d0-436f-b40b-a369f00748f6')
+    ('7e46fff2-4948-11ec-81d3-0242ac130003', '2021-11-18 03:31:00', '21ea2a5a-a7d0-436f-b40b-a369f00748f6');
+
+insert into monPetitBonsai.pruning(id, datetime, bonsai_id) VALUES
+    ('f9be6bce-4949-11ec-81d3-0242ac130003', '2021-11-18 03:40:00', '63696432-01a2-4b43-b162-47b4f1e6062a');
