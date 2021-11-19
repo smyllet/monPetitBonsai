977033fdb0320e9d7c9fa5df85c030c240176e41
Smyllet <smyllet.gamer@gmail.com>
2021-11-19 16:25:45 +0100

    feat : add owner model

---
+++ src/main/java/fr/bryanprolong/monpetitbonsai/owner/domain/model/Owner.java
@@ -0,0 +1,39 @@
+package fr.bryanprolong.monpetitbonsai.owner.domain.model;
+
+import fr.bryanprolong.monpetitbonsai.bonsai.domain.model.Bonsai;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Owner {
+    private UUID id;
+    private String name;
+    private List<Bonsai> bonsais;
+
+    public Owner() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Bonsai> getBonsais() {
+        return bonsais;
+    }
+
+    public void setBonsais(List<Bonsai> bonsais) {
+        this.bonsais = bonsais;
+    }
+}
