9bec8f4d317f4a6fa3525fa55576c9996b99959a
Smyllet <smyllet.gamer@gmail.com>
2021-11-19 16:02:41 +0100

    feat : sql : watering

--- src/main/resources/sql/createTable.sql
+++ src/main/resources/sql/createTable.sql
@@ -1,4 +1,6 @@
+drop table if exists monPetitBonsai.watering;
 drop table if exists monPetitBonsai.bonsai;
+drop table if exists monPetitBonsai.owner;
 drop schema if exists monPetitBonsai;
 
 create schema monPetitBonsai;
@@ -19,8 +21,22 @@ create table monPetitBonsai.bonsai (
    foreign key (owner_id) references monPetitBonsai.owner(id)
 );
 
+create table monPetitBonsai.watering (
+    id UUID primary key not null,
+    datetime timestamp not null,
+    bonsai_id UUID not null,
+    foreign key (bonsai_id) references monPetitBonsai.bonsai(id)
+);
+
 insert into monPetitBonsai.owner(id, name) values ('a3387036-4946-11ec-81d3-0242ac130003', 'Bryan'), ('ae1f28b4-4946-11ec-81d3-0242ac130003', 'Nahoufane');
 
 insert into monPetitBonsai.bonsai(id, name, species, acquisition_date, acquisition_age, owner_id, status) values
     ('63696432-01a2-4b43-b162-47b4f1e6062a', 'Mathilde', 'Jade tree', '10-06-2021', 25, 'a3387036-4946-11ec-81d3-0242ac130003', 'alive'),
-    ('21ea2a5a-a7d0-436f-b40b-a369f00748f6', 'Théodore', 'Poivrier du japon', '02-07-2008', 9, 'ae1f28b4-4946-11ec-81d3-0242ac130003', 'alive')
+    ('21ea2a5a-a7d0-436f-b40b-a369f00748f6', 'Théodore', 'Poivrier du japon', '02-07-2008', 9, 'ae1f28b4-4946-11ec-81d3-0242ac130003', 'alive');
+
+insert into monPetitBonsai.watering(id, datetime, bonsai_id) VALUES
+    ('7e46fa0c-4948-11ec-81d3-0242ac130003', '2021-11-02 13:32:00', '63696432-01a2-4b43-b162-47b4f1e6062a'),
+    ('7e46fc96-4948-11ec-81d3-0242ac130003', '2021-11-08 15:44:00', '63696432-01a2-4b43-b162-47b4f1e6062a'),
+    ('7e46fe08-4948-11ec-81d3-0242ac130003', '2021-11-08 22:19:00', '21ea2a5a-a7d0-436f-b40b-a369f00748f6'),
+    ('7e46fed0-4948-11ec-81d3-0242ac130003', '2021-11-15 05:54:00', '63696432-01a2-4b43-b162-47b4f1e6062a'),
+    ('7e46fff2-4948-11ec-81d3-0242ac130003', '2021-11-18 03:31:00', '21ea2a5a-a7d0-436f-b40b-a369f00748f6')
