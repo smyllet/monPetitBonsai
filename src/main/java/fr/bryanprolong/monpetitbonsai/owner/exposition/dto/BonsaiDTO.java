3ad406dcef1447a7f3b5165adb48e32d4952ad55
Smyllet <smyllet.gamer@gmail.com>
2021-12-01 13:15:12 +0100

    feat : add BonsaiDTO in owner package

---
+++ src/main/java/fr/bryanprolong/monpetitbonsai/owner/exposition/dto/BonsaiDTO.java
@@ -0,0 +1,48 @@
+package fr.bryanprolong.monpetitbonsai.owner.exposition.dto;
+
+import java.util.UUID;
+
+public class BonsaiDTO {
+    private UUID id;
+
+    private String name;
+
+    private String species;
+
+    private int age;
+
+    public BonsaiDTO() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
